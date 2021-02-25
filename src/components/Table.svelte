f455396b1b500cad937f0ebdd20fb9da4c3534de
royghu201901 <50114990+royghu201901@users.noreply.github.com>
2021-02-26 00:16:46 +0800

    try to add unit number

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -29,11 +29,39 @@
     // 或者在标签页直接像现在这样用svelte语法，直接在便签中添加stopPropagation
     store.remove(id)
   }
+
+  export let unit = 1
+
+  function minus() {
+    if (unit > 1) {
+      unit -= 1
+    }
+  }
+
+  function add() {
+    unit += 1
+  }
 </script>
 
 <style>
   table {
     width: 100%;
+  }
+  .button-box {
+    display: flex;
+  } 
+  .two {
+    /* width: 2rem; */
+    text-align: center;
+    margin: 0 1rem;
+    /* display: inline-block; */
+    vertical-align: middle;
+  }
+  .unit-input {
+    width: 10rem;
+    margin-bottom: 0;
+    /* display: inline-block; */
+    vertical-align: middle;
   }
   .order {
     cursor: pointer;
@@ -45,6 +73,7 @@
     <tr>
       <th>Item</th>
       <th>Price</th>
+      <th>Unit</th>
       <th />
     </tr>
   </thead>
@@ -53,6 +82,30 @@
     <tr class="order" on:click={edit(order.id, order.order, order.price)}>
       <td>{order.order}</td>
       <td>{currencyFormat.format(order.price)}</td>
+      <td class="button-box">
+        <div
+          class="two columns"
+          on:click|stopPropagation={minus}
+          type="button"
+        >
+          -
+      </div>
+        <input
+          bind:value={unit}
+          class="unit-input"
+          min="0"
+          step="any"
+          type="text"
+          name="unit"
+        >
+        <div
+          class="two columns"
+          on:click|stopPropagation={add}
+          type="button"
+        >
+          +
+        </div>
+      </td>
       <td>
         <i
           class="far fa-trash-alt"
@@ -63,7 +116,7 @@
     {/each}
     <tr>
       <td>Total</td>
-      <td colspan="2">{currencyFormat.format(total)}</td>
+      <td colspan="3">{currencyFormat.format(total)}</td>
     </tr>
   </tbody>
 </table>
